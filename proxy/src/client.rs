cd0478aa03f8a4eed9c4f4f5122bdbb8c61a0043
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2023-01-04 15:10:32 +1100

    add in client minor adjustments WIP

--- proxy/src/client.rs
+++ proxy/src/client.rs
@@ -10,6 +10,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = GreeterClient::connect("http://[::1]:50051").await?;
 
     let request = tonic::Request::new(ExecuteRequest {
+        world_address: "0x1234".into(),
         system_guid: "world.system.example".into(),
         entity: "1234".into(),
         data_len: 0,
--- proxy/src/server.rs
+++ proxy/src/server.rs
@@ -19,42 +19,56 @@ pub struct MyGreeter {}
 impl Greeter for MyGreeter {
     async fn execute(
         &self,
-        _request: Request<ExecuteRequest>, // Accept request of type HelloRequest
+        request: Request<ExecuteRequest>,
     ) -> Result<Response<ExecuteResponse>, Status> {
-        // Return an instance of type HelloReply
+        let inner_request = request.into_inner().clone();
 
+        let devnet_provider = SequencerGatewayProvider::starknet_nile_localhost();
 
-        let reply = hello_world::ExecuteResponse {
-            success: true, // We must use .into_inner() as the fields of gRPC requests and responses are private
-        };
+        let testnet_provider = SequencerGatewayProvider::starknet_nile_localhost();
 
+        let call_data = vec![
+            FieldElement::from_hex_be(&inner_request.system_guid).unwrap(),
+            FieldElement::from_hex_be(&inner_request.entity).unwrap(),
+            FieldElement::from_hex_be(&inner_request.data_len).unwrap(),
+            // data
+        ];
 
-        // TODO: This is where we forward txs from the client to the devenet AND starknet
-
-        // Placeholders as example
-
-        let provider = SequencerGatewayProvider::starknet_nile_localhost();
-        let tst_token_address =
-            felt!("0x07394cbe418daa16e42b87ba67372d4ab4a5df0b05c6e554d158458ce245bc10");
-
-        let call_result = provider
+        let dev_call_result = devnet_provider
             .call_contract(
                 CallFunction {
-                    contract_address: tst_token_address,
-                    entry_point_selector: selector!("balanceOf"),
-                    calldata: vec![FieldElement::from_hex_be(
-                        "0x0380F7644A98f9D9915dBAa0bbD4B3fe8671A46fBf9f9ab7A7B1DC3b7Ce9EC72",
-                    )
-                    .unwrap()],
+                    contract_address: FieldElement::from_hex_be(&inner_request.system_guid)
+                        .unwrap(),
+                    entry_point_selector: selector!("execute"),
+                    calldata: (*call_data).to_vec(),
                 },
                 BlockId::Latest,
             )
             .await
             .expect("failed to call contract");
 
+        let call_result = testnet_provider
+            .call_contract(
+                CallFunction {
+                    contract_address: FieldElement::from_hex_be(&inner_request.system_guid)
+                        .unwrap(),
+                    entry_point_selector: selector!("execute"),
+                    calldata: (*call_data).to_vec(),
+                },
+                BlockId::Latest,
+            )
+            .await
+            .expect("failed to call contract");
+
+        dbg!(dev_call_result);
+
         dbg!(call_result);
 
         println!("Call indexer");
+
+        let reply = hello_world::ExecuteResponse {
+            success: true, // We must use .into_inner() as the fields of gRPC requests and responses are private
+        };
 
         Ok(Response::new(reply)) // Send back our formatted greeting
     }
