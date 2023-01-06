5d227bdb57a785ba67bde48496f7e2f71030c52c
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2023-01-06 12:21:51 +1100

    (feat): server and client hack WIP

--- proxy/src/client.rs
+++ proxy/src/client.rs
@@ -4,32 +4,53 @@ use std::vec;
 
 use hello_world::greeter_client::GreeterClient;
 use hello_world::ExecuteRequest;
+
 use starknet::core::crypto::ecdsa_sign;
-use starknet::core::types::FieldElement;
+use starknet::core::types::{CallFunction, FieldElement};
+use starknet::macros::selector;
+use starknet::providers::{Provider, SequencerGatewayProvider};
 
 pub mod hello_world {
-    tonic::include_proto!("proxy");
+    tonic::include_proto!("helloworld");
 }
 
 // Account #0 - NOTE: This is just devnet account, not a real one
-const ADDRESS: &str = "0x605d49242d6d1476d93f2282900e05ec3120f796b03f238bd8a339005363d49";
+pub const ADDRESS: &str = "0x605d49242d6d1476d93f2282900e05ec3120f796b03f238bd8a339005363d49";
+
 // const PUBLIC: &str = "0x2a4433c15f014f4b8db9ae3eb8ba85a089ac22fdcc0c38e534dc8a2c471572f";
-const PRIVATE: &str = "0x6d1fa9062223bccdb8db99ff53b8d820";
+pub const PRIVATE: &str = "0x6d1fa9062223bccdb8db99ff53b8d820";
 
-const NONCE: &str = "1";
-const MAX_FEE: &str = "1";
+pub const NONCE: &str = "1";
 
-const TESTNET_ETH_ADDRESS: &str =
+pub const MAX_FEE: &str = "1";
+
+pub const TESTNET_ETH_ADDRESS: &str =
     "0x49D36570D4E46F48E99674BD3FCC84644DDD6B96F7C741B1562B82F9E004DC7";
 
-const SELECTOR: &str = "transfer";
+pub const SELECTOR: &str = "transfer";
 
-const ADDRESS_2: &str = "0x6e65ff327121a4fdfacc538bf5b8abef39015aad7777a0295063a1fc66829a7";
+pub const ADDRESS_2: &str = "0x6e65ff327121a4fdfacc538bf5b8abef39015aad7777a0295063a1fc66829a7";
 
 // testing transfer on testnet
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let provider = SequencerGatewayProvider::starknet_nile_localhost();
+
+    let call_result = provider
+        .call_contract(
+            CallFunction {
+                contract_address: FieldElement::from_hex_be(TESTNET_ETH_ADDRESS).unwrap(),
+                entry_point_selector: selector!("balanceOf"),
+                calldata: vec![FieldElement::from_hex_be(ADDRESS_2).unwrap()],
+            },
+            starknet::core::types::BlockId::Latest,
+        )
+        .await
+        .expect("failed to call contract");
+
+    dbg!(call_result);
+
     let mut client = GreeterClient::connect("http://[::1]:50051").await?;
 
     let signature = ecdsa_sign(
@@ -58,6 +79,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let response = client.execute(request).await?;
 
     println!("RESPONSE={:?}", response);
+
+    // get Balance
+
+    let call_result = provider
+        .call_contract(
+            CallFunction {
+                contract_address: FieldElement::from_hex_be(TESTNET_ETH_ADDRESS).unwrap(),
+                entry_point_selector: selector!("balanceOf"),
+                calldata: vec![FieldElement::from_hex_be(ADDRESS_2).unwrap()],
+            },
+            starknet::core::types::BlockId::Latest,
+        )
+        .await
+        .expect("failed to call contract");
+
+    dbg!(call_result);
 
     Ok(())
 }
--- proxy/src/server.rs
+++ proxy/src/server.rs
@@ -1,5 +1,5 @@
 use starknet::{
-    core::types::FieldElement,
+    core::{crypto::ecdsa_sign, types::FieldElement},
     providers::jsonrpc::{
         models::{BroadcastedInvokeTransaction, BroadcastedInvokeTransactionV1},
         HttpTransport, JsonRpcClient,
@@ -15,6 +15,21 @@ use url::Url;
 pub mod hello_world {
     tonic::include_proto!("helloworld"); // The string specified here must match the proto package name
 }
+
+// Account #0 - NOTE: This is just devnet account, not a real one
+pub const ADDRESS: &str = "0x605d49242d6d1476d93f2282900e05ec3120f796b03f238bd8a339005363d49";
+// const PUBLIC: &str = "0x2a4433c15f014f4b8db9ae3eb8ba85a089ac22fdcc0c38e534dc8a2c471572f";
+const PRIVATE: &str = "0x6d1fa9062223bccdb8db99ff53b8d820";
+
+const NONCE: &str = "1";
+const MAX_FEE: &str = "1";
+
+const TESTNET_ETH_ADDRESS: &str =
+    "0x49D36570D4E46F48E99674BD3FCC84644DDD6B96F7C741B1562B82F9E004DC7";
+
+const SELECTOR: &str = "transfer";
+
+const ADDRESS_2: &str = "0x6e65ff327121a4fdfacc538bf5b8abef39015aad7777a0295063a1fc66829a7";
 
 #[derive(Debug, Default)]
 pub struct MyGreeter {}
@@ -33,21 +48,27 @@ impl Greeter for MyGreeter {
     ) -> Result<Response<ExecuteResponse>, Status> {
         let inner_request = request.into_inner();
 
+        let signature = ecdsa_sign(
+            &FieldElement::from_hex_be(PRIVATE).unwrap(),
+            &FieldElement::from_hex_be(
+                "06fea80189363a786037ed3e7ba546dad0ef7de49fccae0e31eb658b7dd4ea76",
+            )
+            .unwrap(),
+        )
+        .unwrap();
+
         let proxy_local_tx = &BroadcastedInvokeTransaction::V1(BroadcastedInvokeTransactionV1 {
             max_fee: FieldElement::ONE,
-            signature: vec![
-                FieldElement::from_hex_be(
-                    "156a781f12e8743bd07e20a4484154fd0baccee95d9ea791c121c916ad44ee0",
-                )
-                .unwrap(),
-                FieldElement::from_hex_be(
-                    "7228267473c670cbb86a644f8696973db978c51acde19431d3f1f8f100794c6",
-                )
-                .unwrap(),
-            ],
+            signature: vec![signature.r, signature.s],
             nonce: FieldElement::from_hex_be(&inner_request.nonce).unwrap(),
             sender_address: FieldElement::from_hex_be(&inner_request.sender_address).unwrap(),
-            calldata: vec![FieldElement::from_hex_be("1").unwrap()],
+            calldata: vec![
+                FieldElement::from_hex_be(TESTNET_ETH_ADDRESS).unwrap(),
+                FieldElement::from_hex_be(SELECTOR).unwrap(),
+                FieldElement::from_hex_be(ADDRESS_2).unwrap(),
+                FieldElement::from_hex_be("1000000000000000").unwrap(),
+                FieldElement::from_hex_be("0").unwrap(),
+            ],
         });
 
         let rpc_client = create_jsonrpc_client();
